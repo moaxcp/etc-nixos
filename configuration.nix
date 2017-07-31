4816ca77b70ee3622a1a5aebf3f1b23813a2c5f8
John Mercier <moaxcp@gmail.com>
2017-07-30 23:13:05 -0400

    attempting to add default Xresources. These contain colors for xterm.

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports = [ # Include the results of the hardware scan.
@@ -57,6 +57,9 @@
     defaultLocale = "en_US.UTF-8";
   };
 
+  fonts.fonts = with pkgs; [ terminus_font ];
+  fonts.enableDefaultFonts = true;
+
   # Set your time zone.
   time.timeZone = "US/Eastern";
 
@@ -64,6 +67,8 @@
   # $ nix-env -qaP | grep wget
    environment.systemPackages = with pkgs; [
     obnam
+    terminus_font
+    terminus_font_ttf
   ];
 
   networking.hostName = "n1";
@@ -122,6 +127,10 @@
   # Enable the KDE Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sessionCommands = 
+    lib.mkIf config.services.xserver.enable ''
+      '${pkgs.xorg.xrdb}/bin/xrdb' -load ${./Xresources}
+    '';
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.john = {
@@ -136,6 +145,43 @@
   programs.java.enable = true;
   programs.bash = {
     enableCompletion = true;
-    
-  };  
+    promptInit = ''
+      #change PS1 to use colors and optimize space used. PS1 will show status of previous command.
+      set_prompt () {
+        Last_Command=$? # Must come first!
+        Blue='\[\e[01;34m\]'
+        White='\[\e[01;37m\]'
+        Red='\[\e[01;31m\]'
+        Green='\[\e[01;32m\]'
+        Reset='\[\e[00m\]'
+        FancyX='-'
+        Checkmark='+'
+
+        # Add a bright white exit status for the last command
+        PS1="$White\$? "
+        # If it was successful, print a green check mark. Otherwise, print
+        # a red X.
+        if [[ $Last_Command == 0 ]]; then
+            PS1+="$Green$Checkmark "
+        else
+            PS1+="$Red$FancyX "
+        fi
+        # If root, just print the host in red. Otherwise, print the current user
+        # and host in green.
+        if [[ $EUID == 0 ]]; then
+            PS1+="$Red\\h "
+        else
+            PS1+="$Green\\u@\\h "
+        fi
+        # Print the working directory and prompt marker in blue, and reset
+        # the text color to the default.
+        PS1+="$Blue\\W \\\$$Reset "
+      }
+      export HISTCONTROL=ignoredups:erasedups
+      export HISTSIZE=10000
+      export HISTFILESIZE=100000
+      shopt -s histappend
+      export PROMPT_COMMAND="history -a; history -c; history -r; set_prompt; $PROMPT_COMMAND"
+    '';
+  };
 }
