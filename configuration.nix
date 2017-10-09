0f195721c8f9967139475648d7086a033e55518a
John Mercier <moaxcp@gmail.com>
2017-10-09 14:16:04 -0400

    moving obnam to obnam.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports = [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./package-upgrades.nix
+      ./obnam.nix
       ./bash.nix
       ./user/john.nix
     ];
@@ -137,32 +138,6 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
-
-  systemd.services.obnam = {
-    description = "system backup with obnam";
-    script = "PATH=${pkgs.obnam}/bin:${pkgs.coreutils}/bin ${pkgs.stdenv.shell} ${./obnam.sh}";
-  };
-
-  systemd.timers.obnam = {
-    description = "run obnam every day";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
-    };
-  };
-
-  environment.etc."obnam.conf" = {
-    enable = true;
-    text = ''
-      [config]
-      repository = /data/obnam/all
-      client-name = n1
-      root = /
-      one-file-system = yes
-      exclude = /data
-    '';
-  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
