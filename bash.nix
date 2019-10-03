24c0bcbdd94174df3ce88d82c483597b708ea0d2
John Mercier <moaxcp@gmail.com>
2019-10-03 13:02:44 -0400

    Update bash.nix

--- bash.nix
+++ bash.nix
@@ -6,25 +6,12 @@
     promptInit = ''
       #change PS1 to use colors and optimize space used. PS1 will show status of previous command.
       set_prompt () {
-        local Last_Command="$?"
-
-        local Blue='\[\e[01;34m\]'
-        local White='\[\e[01;37m\]'
-        local Red='\[\e[01;31m\]'
-        local Green='\[\e[01;32m\]'
-        local Reset='\[\e[00m\]'
-        local FancyX='✘'
-        local Checkmark='✔'
-
-        PS1=""
-
-        if [[ "$Last_Command" == 0 ]]; then
-            PS1+="$Green$Checkmark"
+        if[[ "$?" == 0 ]]; then
+          PS1=""
         else
-            PS1+="$Red$FancyX $Red($Last_Command)"
+          PS1="\[\e[01;31m\]($?) "
         fi
-
-        PS1+=" $Blue\\\$$Reset "
+        PS1+="\[\e[01;;34m\]\\\$\[\e[00m\] "
       }
 
       export HISTCONTROL=ignoredups:erasedups
